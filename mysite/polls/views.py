8d7ccf4566ca15882f511141d1300e41ecb11f2e
Kamil <kamil.jankowski@profil-software.com>
2024-06-10 14:19:44 +0200

    end part 6

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,10 +1,11 @@
-from datetime import timezone
+
 
 from django.db.models import F
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 from .models import Choice, Question
 
